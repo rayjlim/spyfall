1b1345e069897af7e8207e7941069a8e1d60c39d
Ray Lim <rayjlim1@gmail.com>
2014-12-28 01:08:35 -0800

    change main js files to a cleaner prototype format move game round and init to be a public var, which removes the necessity for a couple of methods reduce redundant code in the choose randomweight

--- tests/SpyGame.test.js
+++ tests/SpyGame.test.js
@@ -1,26 +1,24 @@
 // beforeEach(function(done){});
 describe("SpyGame", function() {
- 
+
   describe("getPlayerRole", function() {
     it("return spy for spy", function() {
 
       var testGame = new Spy.Game();
-      var newround = {
-        'spy': '1',
-        'cPlayer': '1'
+      testGame.round = {
+        'spy': 1,
+        'cPlayer': 1
       };
-      testGame.setRound(newround);
       expect(testGame.getPlayerRole()).to.equal('You\'re the Spy');
     });
     it("returns location  for non-spy", function() {
 
       var testGame = new Spy.Game();
-      var newround = {
-        'spy': '1',
-        'cPlayer': '2',
+      testGame.round = {
+        'spy': 1,
+        'cPlayer': 2,
         'location': 'Test'
       };
-      testGame.setRound(newround);
       expect(testGame.getPlayerRole()).to.equal('Test');
     });
   });
@@ -29,18 +27,35 @@ describe("SpyGame", function() {
     it("should set the basics", function() {
 
       var testGame = new Spy.Game();
-      var newround = {
-        'number': '2',
-        'spy': '1',
-        'cPlayer': '1'
+      testGame.round = {
+        'number': 2,
+        'spy': 1,
+        'cPlayer': 1
       };
-      testGame.setRound(newround);
       testGame.generateRound();
-      var newRound = testGame.getRound();
-      expect(newRound.number).to.equal(3);
-      expect(newRound.cPlayer).to.equal(0);
-      console.log('newRound');
-      console.log(newRound);
+
+      expect(testGame.round.number).to.equal(3);
+      expect(testGame.round.cPlayer).to.equal(0);
+
+    });
+  });
+  describe("getBasicInfo", function() {
+    it("should return object of basics", function() {
+
+      var testGame = new Spy.Game();
+      testGame.round = {
+        'number': 2,
+        'spy': 1,
+        'cPlayer': 1,
+        'numberOfPlayers': 5
+
+      };
+      var actual = testGame.getBasicInfo();
+
+      expect(actual.playerName).to.equal('Player 2');
+      expect(actual.roundNumber).to.equal(2);
+      expect(actual.playerCount).to.equal(5);
+
     });
   });
 });
